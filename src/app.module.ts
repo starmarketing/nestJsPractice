7ca00f94aeb2a8af272d6d4e5a494df33aa0eb08
Emad Khan <emadkhanqai@gmail.com>
2022-06-11 02:58:05 +0500

    database connection succeded

---
+++ src/app.module.ts
@@ -0,0 +1,21 @@
+import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm/dist/typeorm.module';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+
+@Module({
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'mssql',
+      host: 'localhost',
+      port: 1433,
+      username: 'test',
+      password: 'test',
+      database: 'attendance',
+      entities: [__dirname + '/**/*.entity{.ts,.js}'],
+    }),
+  ],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
